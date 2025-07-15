64bf5689e8862a69abb7964c652094a19764e1f0
munir <smsirajulmonir@gmail.com>
2025-07-15 21:58:41 +0600

    not found api ready

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,37 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import router from './app/routes';
-
+import cookieParser from 'cookie-parser';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import httpStatus from 'http-status'
 const app: Application = express();
+app.use(cors());
+app.use(cookieParser())
+
 
 // parsers
 app.use(express.json());
-app.use(cors({
-    origin:['http://localhost:5173'],
-    credentials: true
-}));
-
-app.use('/api',router)
+app.use(express.urlencoded({extended:true}));
 
 app.get('/', (req:Request, res:Response) => {
   res.send('Courier API is running!');
 });
+
+app.use('/api',router);
+
+app.use(globalErrorHandler);
+
+app.use((req:Request, res:Response, next:NextFunction) =>{
+  res.status(httpStatus.NOT_FOUND).json({
+    success:false,
+    message:'Api not found!',
+    error:{
+      path: req.originalUrl,
+      message:"Your request path is not found!"
+    }
+  })
+})
+
+
 
 export default app;
